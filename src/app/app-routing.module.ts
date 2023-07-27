88d08f43da5c8c1c76fb39b3cd88e3625e970a96
Francisco Moreno <franciscoj.sparcoltary@gmail.com>
2023-07-26 18:21:26 -0600

    add: legal page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { LegalComponent } from './pages/legal/legal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'legal', component: LegalComponent },
 ];
 
 @NgModule({
